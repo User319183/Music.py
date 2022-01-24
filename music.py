5f8594020241735e131d43dfd5664ea12517c7e6
Sathvik <88944590+User319183@users.noreply.github.com>
2022-01-24 17:25:02 -0500

    Add files via upload

---
+++ music.py
@@ -0,0 +1,5 @@
+from playsound import playsound #play sound on desktop
+import os
+
+#download a .MP4 or .MP3 song and rename the song. Move the song onto this folder.
+playsound('renamed_song_here.mp4') #or .mp3
